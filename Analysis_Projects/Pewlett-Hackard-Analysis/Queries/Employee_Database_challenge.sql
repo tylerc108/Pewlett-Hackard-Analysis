db36331db4ba2cde220d1ea1637930097c454f53
tylerc108 <103055666+tylerc108@users.noreply.github.com>
2022-05-30 20:23:00 -0400

    d2 done

--- Analysis_Projects/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
+++ Analysis_Projects/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -34,4 +34,21 @@ GROUP BY ut.title
 ORDER BY count DESC;
 SELECT * FROM retiring_titles;
 
-DROP TABLE retiring_titles;
+-- Deliverable 2
+SELECT DISTINCT ON (emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+     AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+SELECT * FROM mentorship_eligibility;
